5ff2f8f2c6d5fccc0e05a86e05b47c33da31a6de
Shanel Beebe <shanelbeebe@icloud.com>
2020-05-05 18:07:00 -0400

    working on timers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'; 
-import Modal from './Modal'
-import firebase from './firebase';
-import './styles.css';
+import React, { Component } from "react";
+import Modal from "./Modal";
+import firebase from "./firebase";
+import "./styles.css";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       modalOpen: false,
       title: "",
-      time: "",
+      setTime: "",
       prompts: [],
       selectedPrompt: "",
       userPrompts: [],
-    }
+      // warningTimer: () => {
+      //   setTimeout(() => {
+      //     alert("time is up");
+      //   }, 5000);
+      // },
+    };
   }
 
   componentDidMount() {
@@ -21,30 +26,42 @@ class App extends Component {
       const data = snapshot.val();
       const promptsArray = [];
       for (let key in data) {
-        promptsArray.unshift(data[key])
+        promptsArray.unshift(data[key]);
       }
       this.setState({ userPrompts: promptsArray });
-    })
+    });
   }
 
   setTimer = (e) => {
-    e.preventDefault()
-    // use setTimeout and this.state.time to set interval  
-  }
+    e.preventDefault();
+    // Sets the writing timer according to the user's selection
+    setTimeout(() => {
+      alert("hi");
+    }, this.state.setTime);
+  };
 
-  getFormSelection = (e) => { 
+  getFormSelection = (e) => {
     this.setState({
-      time: e.target.value
-    })
-  }
+      setTime: e.target.value,
+    });
+  };
 
-  saveTitle = (e) => { 
-    this.saveText(e, "title")
-  }
+  saveTitle = (e) => {
+    this.saveText(e, "title");
+  };
 
   saveMessage = (e) => {
-    this.saveText(e, "message")
-  }
+    // Set the 15sec warning timer once user starts the writing timer
+    this.saveText(e, "message");
+  };
+
+  stopTime = () => {
+    // clearTimeout(this.state.warningTimer);
+  };
+
+  startTime = () => {
+    // this.state.warningTimer();
+  };
 
   saveText = (e, typeOfText) => {
     switch (typeOfText) {
@@ -61,66 +78,83 @@ class App extends Component {
       default:
         console.log("no text state to save found");
     }
-    console.log(this.state.title)
-  }
+    console.log(this.state.title);
+  };
 
   toggleModal = () => {
     this.setState({
-      modalOpen: !this.state.modalOpen
-    })
-  } 
+      modalOpen: !this.state.modalOpen,
+    });
+  };
 
   selectPrompt = (prompt) => {
     const selectedPrompt = prompt.target.value;
     console.log("Prompt:", selectedPrompt);
     this.setState({ selectedPrompt: selectedPrompt });
     // display the selected prompt for the writer above the writing area
-  }
-
+  };
 
   render() {
     return (
       <div className="App">
-        <header> 
+        <header>
           <h1>Placeholder Title</h1>
           <button className="toggleButton"></button>
         </header>
 
         <main>
           <h2>Prompt of The Day</h2>
-          <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ” </p>
+          <p>
+            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. ”{" "}
+          </p>
           <button onClick={this.toggleModal}>Get User Generated Prompts</button>
-          {this.state.modalOpen 
-          ? <Modal 
-          exitModal={this.toggleModal}
-          selectPrompt={this.selectPrompt}
-          userPrompts={this.state.userPrompts}
-          /> 
-          : null }
+          {this.state.modalOpen ? (
+            <Modal
+              exitModal={this.toggleModal}
+              selectPrompt={this.selectPrompt}
+              userPrompts={this.state.userPrompts}
+            />
+          ) : null}
           <form action="" className="timeSelectForm" onSubmit={this.setTimer}>
             <label htmlFor="intervals">How long do you want to write?</label>
-            <select name="intervals" id="intervals" onChange={this.getFormSelection}>
+            <select
+              name="intervals"
+              id="intervals"
+              onChange={this.getFormSelection}
+            >
               <option value="">Select an option</option>
-              <option value="300000">5 min</option>
+              <option value="5000">5 sec</option>
+              <option value="10000">5 min</option>
             </select>
             <button type="submit">Submit</button>
           </form>
 
-            <div>
-              <p>Selected prompt: {this.state.selectedPrompt}</p>
-              <button>Export to PDF</button>
-              <form action="">
-                <label htmlFor="">Title</label>
-                <input type="text" placeholder="Title" onChange={this.saveTitle}/>
-                <textarea name="" id="" cols="30" rows="10" onChange={this.saveMessage}></textarea>
-                <div className="progressBar"></div>
-              </form>
-            </div>
+          <div>
+            <p>Selected prompt: {this.state.selectedPrompt}</p>
+            <button>Export to PDF</button>
+            <form action="">
+              <label htmlFor="">Title</label>
+              <input
+                type="text"
+                placeholder="Title"
+                onChange={this.saveTitle}
+              />
+              <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                onChange={this.saveMessage}
+                onKeyDown={this.stopTime}
+                onKeyUp={this.startTime}
+              ></textarea>
+              <div className="progressBar"></div>
+            </form>
+          </div>
         </main>
 
-        <footer>
-
-        </footer>
+        <footer></footer>
       </div>
     );
   }
