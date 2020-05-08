fb6aaae6de62eb440cebe77d82be2cf755d01d94
robinnong <robin.nong@hotmail.com>
2020-05-07 21:52:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/storystarter/precache-manifest.ae0c8041c49ac85905c07eabacdde8f7.js"
+  "/storystarter/precache-manifest.ec7d75dbc21054ca95ce6155625eacb4.js"
 );
 
 self.addEventListener('message', (event) => {
