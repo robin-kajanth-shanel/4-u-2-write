54c1fbfb9ec9f664fc9fd6d74a04f31edc975ab5
robinnong <robin.nong@hotmail.com>
2020-05-07 21:33:09 -0400

    Updates

---
+++ precache-manifest.5262b91cf175da894a5b74b63d85efcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f697ef561f818abbee9d03b37d735bd",
+    "url": "/storystarter/index.html"
+  },
+  {
+    "revision": "697236e991fe729ff5ff",
+    "url": "/storystarter/static/css/main.7f3c75ba.chunk.css"
+  },
+  {
+    "revision": "5dfccca6022932068631",
+    "url": "/storystarter/static/js/2.177f9d2d.chunk.js"
+  },
+  {
+    "revision": "7a4ee600985d845136136209f4689ee8",
+    "url": "/storystarter/static/js/2.177f9d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697236e991fe729ff5ff",
+    "url": "/storystarter/static/js/main.1f9cba8c.chunk.js"
+  },
+  {
+    "revision": "f95b82353c0e88606ee8",
+    "url": "/storystarter/static/js/runtime-main.6b2ac721.js"
+  }
+]);
