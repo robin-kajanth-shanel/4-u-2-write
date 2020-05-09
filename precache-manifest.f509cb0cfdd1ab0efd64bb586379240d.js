277f692d8a852cb007249ac65ce8a65101f8d7e5
robinnong <robin.nong@hotmail.com>
2020-05-08 22:09:22 -0400

    Updates

---
+++ precache-manifest.f509cb0cfdd1ab0efd64bb586379240d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd09b19d7fe08eb7bcde5f1aa142ca4",
+    "url": "/4-u-2-write/index.html"
+  },
+  {
+    "revision": "681307e8f69f9cd41cc2",
+    "url": "/4-u-2-write/static/css/main.d9fdca22.chunk.css"
+  },
+  {
+    "revision": "2a18cf9ba1940e617593",
+    "url": "/4-u-2-write/static/js/2.c34cd745.chunk.js"
+  },
+  {
+    "revision": "7a4ee600985d845136136209f4689ee8",
+    "url": "/4-u-2-write/static/js/2.c34cd745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681307e8f69f9cd41cc2",
+    "url": "/4-u-2-write/static/js/main.6dc47fc5.chunk.js"
+  },
+  {
+    "revision": "d174c3694f885067e4ba",
+    "url": "/4-u-2-write/static/js/runtime-main.4abcf31b.js"
+  }
+]);
